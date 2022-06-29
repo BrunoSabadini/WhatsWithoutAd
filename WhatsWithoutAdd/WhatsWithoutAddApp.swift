59601ba8a2925222de7e1af2eda024575249a95d
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-06-29 12:19:38 -0300

    Initial Commit

---
+++ WhatsWithoutAdd/WhatsWithoutAddApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsWithoutAddApp.swift
+//  WhatsWithoutAdd
+//
+//  Created by Bruno Sabadini on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsWithoutAddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
